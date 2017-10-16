7272a57d241d44347ac301aa43d5cda85e8f0c4e
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2017-10-16 11:09:03 +0530

    Add docker save, load, push, and pull bash scripts

---
+++ load.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "Loading customers service docker image..."
+docker load < customers-service-0.2.tar
+
+echo "Loading credits service docker image..."
+docker load < credits-service-0.2.tar
+
+echo "Loading loans service docker image..."
+docker load < loans-service-0.2.tar
+
+echo "Loading loan applications service docker image..."
+docker load < loan-applications-0.2.tar
+
+echo "Loading wso2apim docker image..."
+docker load < wso2apim-2.1.0-v2.tar
+
+echo "Loading wso2apim analytics service docker image..."
+docker load < wso2apim-analytics-2.1.0-v2.tar
+
+echo "Loading mysql docker image..."
+docker load < wso2ei-bps-6.1.1-v2.tar
+
